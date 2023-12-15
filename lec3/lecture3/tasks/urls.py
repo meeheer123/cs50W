65488a272938b6fa418ecb647a6df4b184783042
= <=>
2023-12-15 19:28:47 +0530

    fix namespace collision

--- lec3/lecture3/tasks/urls.py
+++ lec3/lecture3/tasks/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+# this allows us to avoid namespace collision
+# namespace collision occurs when we have 2 urls.py files with same name attribute
+app_name = "tasks"
 urlpatterns =  [
     path("", views.index, name="index"),
     path("add", views.add, name="add")
